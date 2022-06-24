166373b0d592fa6c3054f9e0e81a6cd19390ce09
Rowan Kooij <r.kooij17@gmail.com>
2022-06-24 08:29:42 +0200

    kleine aanpassingen gedaan

--- applicatie/pages/contact.php
+++ applicatie/pages/contact.php
@@ -9,14 +9,27 @@ require_once './modules/head.php';
     <?php require_once './modules/navbar.php'; ?>
     <main>
         <div class="contact">
-            <h2>Contact information:</h2>
-            <ul>
-                <li>Tel: 0318-123456</li>
-                <li>Mail: <a href="mailto:admin@fletnix.nl">admin@fletnix.nl</a></li>
-                <li>Adress: Ruitenberglaan 31</li>
-                <li>City: Arnhem</li>
-                <li>Postalcode: 6826 CC</li>
-            </ul>
+            <div>
+                <h3>Contact information:</h3>
+                <ul>
+                    <li>Tel: 0318-123456</li>
+                    <li>Mail: <a href="mailto:admin@fletnix.nl">admin@fletnix.nl</a></li>
+                    <li>Adress: Ruitenberglaan 31</li>
+                    <li>City: Arnhem</li>
+                    <li>Postalcode: 6826 CC</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Privacyverklaring:</h3>
+                <a href="../assets/privacyverklaring.pdf" download="privacyverklaring fletnix"><button
+                        class="simple-btn">Download</button></a>
+            </div>
+            <div class="about-us">
+                <h3>About us:</h3>
+                <p>At Fletnlix, we want to entertain the world. Whatever your taste, and no matter where you live, we
+                    give you access to best-in-class TV series, documentaries and feature films.</p>
+                <p>Fletnix is made as a project for school but now the wolrd has access to our library!</p>
+            </div>
         </div>
     </main>
 </body>
